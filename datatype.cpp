c0e4c61f48b3abac48e9c06020fbdeea6a8c342d
sonali <sonalirockz15@gmail.com>
2021-10-06 16:57:10 +0530

    uploaded

---
+++ datatype.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//float d=34.4f;
+	//long double e=34.4l;
+	//*******************************float , double and long double datatypes lietrals********************************
+//	cout<<"size of 34.4 is "<<sizeof(34.4)<<endl; //size of double
+//	cout<<"size of 34.4f is "<<sizeof(34.4f)<<endl;  //size of float
+//	cout<<"size of 34.4F is "<<sizeof(34.4F)<<endl; //size of float
+//	cout<<"size of 34.4l is "<<sizeof(34.4l)<<endl; //size of long
+//	cout<<"size of 34.4L is "<<sizeof(34.4L)<<endl; //size of long
+	//*******************************reference variables********************************
+	//float x=63;
+	//float & y=x;
+	//cout<<x<<endl;
+	//cout<<y<<endl;
+	//*******************************type casting********************************
+	int z=36;
+	float s=45.00;
+	cout<<"the value of z is: "<<(float)z<<endl;
+	cout<<"the value of z is: "<<int(s);
+	//int c=int(s);
+}
+
